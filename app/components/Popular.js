5a8dfed88a0fde3a2588bd96d337b888157a3130
sharanr91 <sharanr91@Dear.General.github.com>
2019-06-30 13:48:47 +1000

    Initial Commit

---
+++ app/components/Popular.js
@@ -0,0 +1,57 @@
+var React = require("react");
+var PropTypes = require("prop-types");
+
+function SelectLanguage(props) {
+  var languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <ul className="languages">
+      {languages.map(function(l) {
+        return (
+          <li
+            style={l === props.selectedLanguage ? { color: "#d0021b" } : null}
+            onClick={props.onSelect.bind(null, l)}
+            key={l}
+          >
+            {l}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function() {
+      return { selectedLanguage: lang };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
+    );
+  }
+}
+
+module.exports = Popular;
